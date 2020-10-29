2f6532b0fd6e733bf5901aa0a9a4a4f181afa725
Maximilian Held <info@maxheld.de>
2020-10-29 20:54:34 +0100

    factor out more boilerplate, closes #7

--- R/template.R
+++ R/template.R
@@ -10,15 +10,19 @@
 #'
 #' As a result, your website will:
 #'
-#' - Use the [bootswatch `cosmo` bootstrap theme](https://bootswatch.com/3/cosmo/).
-#' - Set the navbar to `type: inverse`.
+#' - Use the [**bootswatch `cosmo` bootstrap theme**](https://bootswatch.com/3/cosmo/) with an inverse navbar.
+#'   The corresponding settings in you `_pkgdown.yml` (`template: params: bootswatch`, `navbar: type: inverse`) are ignored.
 #' - Display in a University of Göttingen **color scheme**.
 #'    This is determined by the `ugoe_bootswatch.css`, which is compiled from `ugoe_colors.less`.
 #'    The colors were added and downloaded via https://www.bootstrap-live-customizer.com.
-#' - Include the SUB wordmark in your website as `sub_wordmark.jpg`.
-#' - Add a custom footer including the wordmar.
+#' - Include the **SUB wordmark** in your website as `sub_wordmark.jpg`.
+#'    The wordmark is included in the custom footer.
+#' - Expose the SUB twitter account [`@subugoe`](https://twitter.com/subugoe) as **opengraph [metadata](https://pkgdown.r-lib.org/articles/metadata.html)** (`twitter: site`).
+#'
+#' In a further difference to "vanilla" pkgdown sites, Google Analytics is disabled for all subugoetheme sites.
 #'
 #' To pull in changes in this package to your website, install the current version of {subugoetheme} and re-run `pkgdown::build_site()` locally or in your CI.
+#'
 #' To pull in upstream changes from {pkgdown} into *this* package, it may be ocassionally necessary to update the files in `inst/pkgdown/templates` which have been minimally adapted from their original counterparts in {pkgdown}.
 #'
 #' To learn more about pkgdown templates, read [here](https://pkgdown.r-lib.org/reference/build_site.html) or consider [these](https://github.com/ropensci/rotemplate) [examples](https://tidytemplate.tidyverse.org).
@@ -34,7 +38,7 @@ pkgdown_template <- function() {
     assets = list(
       css = fs::path(path_root, "assets", "ugoe_bootswatch.css"),
       logo = fs::path(path_root, "assets", "sub_logo.svg"),
-      wordmark = fs::path(path_root, "assets", "sub_workdmark.jpg")
+      wordmark = fs::path(path_root, "assets", "sub_wordmark.jpg")
     ),
     templates = list(
       footer = fs::path(path_root, "templates", "footer.html"),
