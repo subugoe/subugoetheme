307a71bf2142a9ec174c251a90af08547555cbc8
Maximilian Held <info@maxheld.de>
2020-11-18 10:32:15 +0100

    document matomo #6 closes #5

--- R/template.R
+++ R/template.R
@@ -1,13 +1,16 @@
 #' State- and University Library of Göttingen [pkgdown](http://pkgdown.r-lib.org) template
 #'
-#' @details
-#' To use this template for website, list {subugoetheme} in your `DESCRIPTION` and add to your `_pkgdown.yml`:
+#' @section Usage:
+#' To use this template for your pkgdown website, list {subugoetheme} in your `DESCRIPTION` and add to your `_pkgdown.yml`:
 #'
 #' ```yaml
 #' template:
 #'   package: subugoetheme
-#' ````
+#' ```
+#' 
+#' To update your website, install the current version of {subugoetheme} and re-run `pkgdown::build_site()` locally or in your CI.
 #'
+#' @section Features:
 #' As a result, your website will:
 #'
 #' - Use the [**bootswatch `cosmo` bootstrap theme**](https://bootswatch.com/3/cosmo/) with an inverse navbar.
@@ -18,12 +21,20 @@
 #' - Include the **SUB wordmark** in your website as `sub_wordmark.jpg`.
 #'    The wordmark is included in the custom footer.
 #' - Expose the SUB twitter account [`@subugoe`](https://twitter.com/subugoe) as **opengraph [metadata](https://pkgdown.r-lib.org/articles/metadata.html)** (`twitter: site`).
+#' 
+#' For additional features, see [muggle](http://subugoe.github.io/muggle/).
 #'
-#' In a further difference to "vanilla" pkgdown sites, Google Analytics is disabled for all subugoetheme sites.
+#' @section Analytics:
+#' Instead of Google Analytics, this template includes a tracking code for `matomo.gwdg.de` on every website.
+#' Matomo is an open-source alternative to Google Analytics, here hosted by the [GWDG](http://matomo.gwdg.de).
+#' 
+#' The following SUB employees have access to the analytics: 
+#' - [Najko Jahn](https://www.sub.uni-goettingen.de/kontakt/personen-a-z/personendetails/person/najko-jahn/)
+#' - [Maximilian Held](https://www.sub.uni-goettingen.de/kontakt/personen-a-z/personendetails/person/maximilian-held/)
+#' - `scholcomm-analytics@sub.uni-goettingen.de`, a function account for [WAG](https://www.sub.uni-goettingen.de/kontakt/abteilungen-a-z/abteilungs-und-gruppendetails/abteilunggruppe/wissen-als-gemeingut/) data products.
 #'
-#' To pull in changes in this package to your website, install the current version of {subugoetheme} and re-run `pkgdown::build_site()` locally or in your CI.
-#'
-#' To pull in upstream changes from {pkgdown} into *this* package, it may be ocassionally necessary to update the files in `inst/pkgdown/templates` which have been minimally adapted from their original counterparts in {pkgdown}.
+#' @section Maintenance:
+#' To pull in upstream changes from {pkgdown} into *this* package, it may be ocassionally neccessary to update the files in `inst/pkgdown/templates` which have been minimally adapted from their original counterparts in {pkgdown}.
 #'
 #' To learn more about pkgdown templates, read [here](https://pkgdown.r-lib.org/reference/build_site.html) or consider [these](https://github.com/ropensci/rotemplate) [examples](https://tidytemplate.tidyverse.org).
 #'
